021ebc8fb98920b0355be909dd25c4db5cc545d2
Gunawardhane <yasangawd@gmail.com>
2024-11-22 18:29:51 +0530

    Set the color theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 77, 36, 173),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true, // Enables Material 3 styles
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme(
+          backgroundColor:
+              kColorScheme.primary, // Use primary color for app bar
+          foregroundColor:
+              kColorScheme.onPrimary, // Use onPrimary for text color
+        ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
