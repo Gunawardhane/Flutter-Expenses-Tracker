8464499858e0ab58f556a7a8b152829a2fde8f97
Gunawardhane <yasangawd@gmail.com>
2024-11-23 13:38:28 +0530

    Added Dark Mode

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,29 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 30, 137, 199),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer),
+        ),
+      ),
       theme: ThemeData(
         useMaterial3: true, // Enables Material 3 styles
         colorScheme: kColorScheme,
@@ -36,6 +56,7 @@ void main() {
                   fontSize: 14),
             ),
       ),
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
